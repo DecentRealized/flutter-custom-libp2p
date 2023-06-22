ba9dd81296f4a5e44a584d12f799ea181824ff47
EtashTyagi <etash18@gmail.com>
2023-06-22 16:43:57 +0530

    Windows Fix Try

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <custom_libp2p/custom_lib_p2_p_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  CustomLibP2PPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("CustomLibP2PPlugin"));
+}
